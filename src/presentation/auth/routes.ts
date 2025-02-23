bafdbdb935f5fe883435d85e5c2a7cf0733bcef3
Jacobo <jakobgmr2@gmail.com>
2025-02-22 20:02:05 -0600

    email-service added

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
-import { AuthService } from "../services/auth.service";
+import { AuthService, EmailService } from "../services";
+import { envs } from "../../config";
 
 export class AuthRoutes{
     static get routes(){
         const router = Router();
-        const authService = new AuthService(); // Aqui se manda la inyección del servicio a utilizar
+
+        const emailService = new EmailService(
+            envs.MAILER_SERVICE,
+            envs.MAILER_EMAIL,
+            envs.MAILER_SECRET_KEY,
+            envs.SEND_EMAIL
+        );
+        const authService = new AuthService(emailService); // Aqui se manda la inyección del servicio a utilizar
 
         const controller = new AuthController(authService);
 
--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,9 +1,13 @@
-import { bcryptAdapter, JwtAdapter } from "../../config";
+import { bcryptAdapter, envs, JwtAdapter } from "../../config";
 import { UserModel } from "../../data";
 import { CustomError, LoginUserDto, RegisterUserDto, UserEntity } from "../../domain";
+import { EmailService } from "./email.service";
 
 export class AuthService{
-    constructor(){}
+    // DI
+    constructor(
+        private readonly emailService: EmailService,
+    ){}
 
     public async registerUser(registerUserDto: RegisterUserDto){
         // Validar que no se repitan emails
@@ -18,14 +22,19 @@ export class AuthService{
 
             await user.save();
 
+            // Email de verificación
+            await this.sendEmailValidationLink(user.email);
+
             // JWT <----- para mantener la autenticación del usuario
+            const token = await JwtAdapter.generateToken({id: user.id});
+            if(!token) throw CustomError.internalServer('Error while creating Token');
 
             //? Forma de ocultar el password, Hay Varias
             const {password, ...userEntity} = UserEntity.fromObject(user);
 
             return {
                 user: userEntity,
-                token: 'User Token',
+                token: token,
             };
         } catch (error) {
             throw CustomError.internalServer(`${error}`);
@@ -51,4 +60,45 @@ export class AuthService{
             token,
         };
     }
+
+    public validateEmail = async(token: string)=>{
+        const payload = await JwtAdapter.validateToken(token);
+        if(!payload) throw CustomError.unauthorized('Invalid Token');
+                                    // Solo para decirle a typescript de que incluye un email
+        const {email} = payload as {email: string};
+        if(!email) throw CustomError.internalServer('Email not in token');
+
+        const user = await UserModel.findOne({email});
+        if(!user) throw CustomError.internalServer('Email not exists');
+
+        user.emailValidated = true;
+        await user.save();
+    
+        return true;
+    };
+
+    private sendEmailValidationLink = async(email: string)=>{
+        const token = await JwtAdapter.generateToken({email});
+        if(!token) throw CustomError.internalServer('Error getting token');
+
+        // Link de verificación del usuario
+        const link = `${envs.WEBSERVICE_URL}/auth/validate-email/${token}`;
+        const html = `
+            <h1>Validate your email</h1>
+            <p>Click on the following link to validate your email</p>
+
+            <a href="${link}">Validate your email: ${email}</a>
+        `;
+
+        const emailOptions = {
+            to: email,
+            subject: 'Validate your email',
+            htmlBody: html,
+        };
+
+        const isSent = await this.emailService.sendEmail(emailOptions);
+        if(!isSent) throw CustomError.internalServer('Error sending email');
+
+        return true;
+    };
 }
